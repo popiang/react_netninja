d406e4361a4446f64068dc5d4ca66e53f2a49be3
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-07-27 07:11:07 +0800

    mymoney2

---
+++ my-money2/src/App.js
@@ -0,0 +1,34 @@
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+} from "react-router-dom/cjs/react-router-dom.min";
+
+// pages & components
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Navbar from "./components/Navbar";
+
+function App() {
+    return (
+        <div>
+            <BrowserRouter>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/signup">
+                        <Signup />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
