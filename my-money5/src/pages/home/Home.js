18786bc777c58bda88486969bd015b164354092e
Shahril Bin Mad Shah <shahril.madshah@airselangor.com>
2023-08-15 17:40:01 +0800

    mymoney5

--- my-money5/src/pages/home/Home.js
+++ my-money5/src/pages/home/Home.js
@@ -1,8 +1,15 @@
+import TransactionForm from "./TransactionForm";
+
 // styles
 import styles from "./Home.module.css";
 
 export default function Home() {
-  return (
-	<div>Home</div>
-  )
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>Transaction List</div>
+            <div className={styles.sidebar}>
+                <TransactionForm />
+            </div>
+        </div>
+    );
 }
