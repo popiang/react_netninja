f62cc20f3399e0431d5e0224df5ed9d58a9e4254
Shahril Bin Mad Shah <shahril.madshah@airselangor.com>
2023-08-21 17:30:24 +0800

    mymoney8

---
+++ my-money8/src/pages/signup/Signup.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+// styles
+import styles from "./Signup.module.css";
+
+export default function Signup() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [displayName, setDisplayName] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email, password, displayName	);
+    };
+
+    return (
+        <form className={styles["signup-form"]} onSubmit={handleSubmit}>
+            <h2>Signup</h2>
+            <label>
+                <span>email:</span>
+                <input
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    required
+                />
+            </label>
+            <label>
+                <span>password:</span>
+                <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    required
+                />
+            </label>
+            <label>
+                <span>display name:</span>
+                <input
+                    type="text"
+                    onChange={(e) => setDisplayName(e.target.value)}
+                    value={displayName}
+                    required
+                />
+            </label>
+            <button className="btn">Signup</button>
+        </form>
+    );
+}
