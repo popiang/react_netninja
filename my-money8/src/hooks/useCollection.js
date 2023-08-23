df04b4700e07a62bd5dc151d6fac0ef83efcaeeb
Shahril Bin Mad Shah <shahril.madshah@airselangor.com>
2023-08-23 14:57:59 +0800

    mymoney8 is done

--- my-money8/src/hooks/useCollection.js
+++ my-money8/src/hooks/useCollection.js
@@ -1,24 +1,42 @@
 import { projectFirestore } from "../firebase/config";
-import {useState, useEffect} from "react";
+import { useState, useEffect, useRef } from "react";
 
-export const useCollection = (collection) => {
-	const [error, setError] = useState(null);
-	const [documents, setDocuments] = useState(null);
+export const useCollection = (collection, _query, _orderBy) => {
+    const [error, setError] = useState(null);
+    const [documents, setDocuments] = useState(null);
+	const query = useRef(_query).current;
+	const orderBy = useRef(_orderBy).current;
 
-	useEffect(() => {
-		setError(null);
+    useEffect(() => {
+        setError(null);
 
-		let ref = projectFirestore.collection(collection);
+        let ref = projectFirestore.collection(collection);
 
-		ref.onSnapshot(snapshot => {
-			let results = [];
-			snapshot.docs.forEach(doc => {
-				results.push(doc);
-			});
-			setDocuments(results);
-		})
+		if (query) {
+			ref = ref.where(...query);
+		}
 
-	}, []);
+		if (orderBy) {
+			ref = ref.orderBy(...orderBy);
+		}
 
-	return {documents, error}
-}
+        const unsub = ref.onSnapshot(
+            (snapshot) => {
+                let results = [];
+                snapshot.docs.forEach((doc) => {
+                    results.push({ ...doc.data(), id: doc.id });
+                });
+                setDocuments(results);
+                setError(null);
+            },
+            (error) => {
+                setError(error.message);
+                console.log(error.message);
+            }
+        );
+
+        return () => unsub();
+    }, [collection, query, orderBy]);
+
+    return { documents, error };
+};
